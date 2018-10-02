b2b194b6c36a1324baa675e5d29f8b6d1b686b8b
gtg489w <gtg489w@gmail.com>
2018-10-02 19:39:03 -0400

    Added 404 and mobile nav

--- src/app/containers/main-container/main-container.component.ts
+++ src/app/containers/main-container/main-container.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
-  selector: 'app-main-container',
-  templateUrl: './main-container.component.html',
-  styleUrls: ['./main-container.component.scss']
+	selector: 'app-main-container',
+	templateUrl: './main-container.component.html',
+	styleUrls: ['./main-container.component.scss']
 })
 export class MainContainerComponent implements OnInit {
 
-  constructor() { }
+	navigationOpen: boolean = true;
 
-  ngOnInit() {
-  }
+	constructor(private router: Router) { }
 
+	ngOnInit() {
+		this.router.events.distinctUntilChanged((previous: any, current: any) => {
+			if(current instanceof NavigationEnd) {
+				return previous.url === current.url;
+			}
+			return true;
+		}).subscribe((route: any) => {
+			this.navigationOpen = false;
+		});
+	}
 }
